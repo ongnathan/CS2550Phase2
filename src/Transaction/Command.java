55544c4a77d182ee9e1c712e62034860e2ccd12e
lilonghao720 <lilonghao720@gmail.com>
2015-04-11 17:11:10 -0400

    Add Transaction Package
    This package have the transaction manager.

---
+++ src/Transaction/Command.java
@@ -0,0 +1,10 @@
+package Transaction;
+
+/**
+ * The enumerated type representing the possible commands that can be issued by a script.
+ * @author Nathan Ong and Jeongmin Lee
+ */
+public enum Command
+{
+	READ_ID, READ_AREA_CODE, COUNT_AREA_CODE, INSERT, DELETE_TABLE, COMMIT, ABORT, BEGIN;
+}//end enum Command
--- src/Transaction/TransactionManager.java
+++ src/Transaction/TransactionManager.java
@@ -1,4 +1,4 @@
-package parser;
+package Transaction;
 
 import java.io.BufferedReader;
 import java.io.EOFException;
@@ -6,20 +6,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadPoolExecutor.AbortPolicy;
 
+import parser.Command;
 import data.AreaCode;
 import data.IDNumber;
 import data.Record;
 
-/**
- * The script parser.
- * Reads the script one line at a time.
- * The line can be accessed as many times as necessary.
- * When a new line is read, the previous line is discarded.
- * @author Nathan Ong and Jeongmin Lee
- */
-public class Parser
-{
+public class TransactionManager {
 	private final File file;
 	private final BufferedReader fileReader;	//The file reader
 	
@@ -32,6 +28,9 @@ public class Parser
 	private boolean streamIsClosed;				//Whether or not the stream has been closed due to reaching the end of file or not
 	
 	private boolean error;
+	private boolean TransactionType;
+	private int TID; 
+	private ArrayList<transaction> OPBuffer;
 	
 	/**
 	 * The constructor.
@@ -39,7 +38,7 @@ public class Parser
 	 * @param filename The String representing the file name.
 	 * @throws FileNotFoundException if the file does not exist.
 	 */
-	public Parser(String filename) throws FileNotFoundException
+	public TransactionManager(String filename) throws FileNotFoundException
 	{
 		this(new File(filename));
 	}
@@ -50,13 +49,16 @@ public class Parser
 	 * @param file The File to read from.
 	 * @throws FileNotFoundException if the file does not exist.
 	 */
-	public Parser(File file) throws FileNotFoundException
+	public TransactionManager(File file) throws FileNotFoundException
 	{
 		this.file = file;
 		this.fileReader = new BufferedReader(new FileReader(file));
 		this.lineNumber = 0;
 		this.streamIsClosed = false;
 		this.error = false;
+		this.TransactionType = false;
+		this.TID = -1;
+		this.OPBuffer = new ArrayList<transaction>();
 	}
 	
 	/**
@@ -87,14 +89,24 @@ public class Parser
 		this.lineNumber++;
 		
 		String[] split = line.split(" ");
-		if(split.length < 2 || split.length > 3)
+		if(split.length > 3)
 		{
 			this.error = true;
 			throw new IOException("File is not in the correct format at line " + this.lineNumber + ".");
 		}
+		else if(split.length == 1){
+			if(split[0].equals("C")){
+				this.command = Command.COMMIT;
+			} else if(split[0].equals("A")){
+				this.command = Command.ABORT;
+			} else {
+				this.error = true;
+				throw new IOException("File is not in the correct format at line " + this.lineNumber + ".");
+			}
+		}
 		else if(split.length == 2)
 		{
-			if(!split[0].equals("D"))
+			if(!split[0].equals("D") || !split[0].equals("B"))
 			{
 				this.error = true;
 				throw new IOException("File is not in the correct format at line " + this.lineNumber + ".");
@@ -102,10 +114,24 @@ public class Parser
 //				this.error = "Malformed command at line " + this.lineNumber + "of the script.\n"
 //						+ "Skipping that line.";
 //				return;
+			} else if(split[0].equals("D")){
+				this.command = Command.DELETE_TABLE;
+				this.tableName = split[1];
+				this.value = null;
+			} else if(split[0].equals("B")){
+				this.command = Command.BEGIN;
+				this.value = Integer.getInteger(split[1]);
+				if((int)value == 1){
+					TransactionType = true;
+					TID = 1; // when cpu finished we add it here;
+				} else {
+					TransactionType = false;
+					TID = -1;
+				}
+				
 			}
-			this.command = Command.DELETE_TABLE;
-			this.tableName = split[1];
-			this.value = null;
+			
+			
 		}
 		else
 		{
@@ -135,7 +161,19 @@ public class Parser
 		}//end else
 		this.fullString = line;
 	}//end method()
-	
+	public ArrayList<transaction> getOPBuffer(){
+		ArrayList<transaction> temp = (ArrayList<transaction>) OPBuffer.clone();
+		OPBuffer.clear();
+		TID = -1;
+		return temp;
+		
+	}
+	public void Abort(){
+		TransactionType = false;
+		TID = -1;
+		OPBuffer.clear();
+		
+	}
 	public String getFullString()
 	{
 		return this.fullString;
@@ -192,4 +230,79 @@ public class Parser
 	{
 		return this.file.getName();
 	}
-}//end class Parser
+	public transaction getTransaction(){
+		
+		transaction temp =  new transaction(command,tableName,value,fullString,lineNumber,TransactionType,TID);
+		if(TransactionType){
+			OPBuffer.add(temp);
+		}
+		return temp;
+	}
+	
+	public class transaction{
+		private Command command;					//The command of the last read line
+		private String tableName;					//The name of the table referenced by the last read line
+		private Object value;						//The value of the last read line.  The type depends on what command is being used
+		private String fullString;
+		
+		private int lineNumber;
+		private boolean TransactionType;
+		private int TID; 
+		public transaction(Command command, String tableName, Object value, String fullString, int lineNumber, boolean TransactionType, int TID){
+			this.command = command;
+			this.tableName = tableName;
+			this.value = value;
+			this.fullString = fullString;
+			this.lineNumber = lineNumber;
+			this.TransactionType = TransactionType;
+			this.TID = TID;
+		}
+		public String getFullString()
+		{
+			return this.fullString;
+		}
+		
+		/**
+		 * Retrieves the Command at the loaded line.
+		 * @return Returns the Command that was issued by the script at the loaded line.
+		 */
+		public Command getCommand()
+		{
+			return this.command;
+		}
+		
+		/**
+		 * Retrieves the table name at the loaded line.
+		 * @return Returns the String representing the table name that was issued by the script at the loaded line.
+		 */
+		public String getTableName()
+		{
+			return this.tableName;
+		}
+		
+		/**
+		 * Retrieves the record at the loaded line.
+		 * @return Returns the Record representing the value that was issued by the script at the loaded line.
+		 */
+		public Object getValue()
+		{
+			return this.value;
+		}
+		
+		/**
+		 * Retrieves the line number that will be read next if loadNextLine() is called.
+		 * @return Returns the integer representing the current position of the cursor in the file.
+		 */
+		public int getLineNumber()
+		{
+			return this.lineNumber;
+		}
+		public boolean getTransactionType()
+		{
+			return this.TransactionType;
+		}
+		public int getTID(){
+			return this.TID;
+		}
+	}
+}
